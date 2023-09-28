af1398b55e450319780a4a552316c5784f718794
Pablo Díaz Hernández <78272596+PabloDiazHernandez@users.noreply.github.com>
2023-09-28 20:43:14 +0200

    Add files via upload

---
+++ entregas/Funkos/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // H2, solo usa una
+    implementation("com.h2database:h2:2.2.224")
+    // Ibatis lo usaremos para leer los scripts SQL desde archivos
+    implementation("org.mybatis:mybatis:3.5.13")
+    // Lombook para generar código, poner todo esto para que funcione
+    implementation("org.projectlombok:lombok:1.18.28")
+    testImplementation("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+    // Logger
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    // Para nuestros test
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    // Mockito para nuestros test con JUnit 5
+    testImplementation("org.mockito:mockito-junit-jupiter:5.5.0")
+    testImplementation("org.mockito:mockito-core:5.5.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
